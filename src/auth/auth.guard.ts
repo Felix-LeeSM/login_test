88a6898ed76eaa0884d40bfe7a2811d50a59b62a
Seungmin <haradwaith@naver.com>
2022-03-16 13:58:30 +0900

    [test]

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -5,7 +5,7 @@ export class LocalAuthGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const req = context.getArgByIndex(0);
     const thisOne = context.getType();
-    console.log('headers', req.header());
+    console.log(req.get());
     console.log('req', req);
     console.log(
       '---------------------------------------------------------------------',
