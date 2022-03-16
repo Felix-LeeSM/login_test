054cc69f56def322dab40ae48f09d39105d9c038
Seungmin <haradwaith@naver.com>
2022-03-16 16:43:23 +0900

    [test]

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,20 +1,9 @@
-import { Request } from 'express';
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class LocalAuthGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const req = context.getArgByIndex(0);
-    console.log(req instanceof Request);
-    console.log(
-      '---------------------------------------------------------------------',
-    );
-    console.log(
-      '---------------------------------------------------------------------',
-    );
-    console.log(
-      '---------------------------------------------------------------------',
-    );
     console.log('rawHeaders', req.rawHeaders);
     console.log(
       '---------------------------------------------------------------------',
